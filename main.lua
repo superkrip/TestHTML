f02cf9621ab816571923aef4a6e597e53b79bd89
murkrow92 <ducdt1992@gmail.com>
2017-07-11 22:02:04 +0700

    + add main

---
+++ main.lua
@@ -0,0 +1,10 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Administrator
+-- Date: 7/11/2017
+-- Time: 10:01 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+print("Hello World");
+
